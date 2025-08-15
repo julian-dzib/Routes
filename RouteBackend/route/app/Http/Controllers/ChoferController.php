ca6a984a6f5d4b560cd8cbc31c873a71f31e2cde
dzibfer6 <6256@itescam.edu.mx>
2025-08-15 00:53:34 -0600

    add validation for NOMBRE and TELEFONO in ChoferController using regular expression (POST and UPDATE)

--- RouteBackend/route/app/Http/Controllers/ChoferController.php
+++ RouteBackend/route/app/Http/Controllers/ChoferController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Choferes;
-
+use Illuminate\Validation\ValidationException;
 
 class ChoferController extends Controller
 {
@@ -42,31 +42,45 @@ class ChoferController extends Controller
      */
     public function store(Request $request)
     {
-        //Validar los datos de entrada
-        $request->validate([
-            "NOMBRE" => "required|string|max:100",
-            "TELEFONO" => "string",
-        ]);
         //Capturar el error si ocurre
         try {
+            //Validar los datos de entrada
+            $request->validate([
+                "NOMBRE" => "required|string|max:100",
+                "TELEFONO" => [
+                    'required',
+                    "regex:/^(\+52)?\s?(1)?\s?[2-9]\d{1}\d{8}$/"
+                ],
+            ],[
+                "NOMBRE.required" => "El campo nombre es obligatorio",
+                "TELEFONO.required" => "El campo número de teléfono es obligatorio",
+                "TELEFONO.regex" => "El número de teléfono no tiene un formato válido"
+            ]
+            );
+
             $drivers= new Choferes();
             $drivers->NOMBRE= $request->input('NOMBRE');
             $drivers->TELEFONO= $request->input('TELEFONO');
             $drivers->save();
 
-            //Retornar un mensaje json
+            //Retornar un mensaje json en caso de que todo salieria bien
             return response()->json([
                 'message' => 'El chofer ha sido creado',
                 'data' => $drivers
             ], 201);
+
+        }catch (ValidationException $e) {
+        // Retornar el error de validacion
+        return response()->json([
+            'errors' => $e->errors()
+        ], 422);
+
         } catch (\Exception $e) {
             // Retornar un mensaje de error
             return response()->json([
                 'error' => 'Error al crear el chofer'
             ], 500);
         }
-
-
     }
 
     /**
@@ -97,17 +111,32 @@ class ChoferController extends Controller
             // Validar los datos de entrada nuevamene
             $request->validate([
                 "NOMBRE" => "required|string|max:100",
-                "TELEFONO" => "string",
-            ]);
+                "TELEFONO" => [
+                    'required',
+                    "regex:/^(\+52)?\s?(1)?\s?[2-9]\d{1}\d{8}$/"
+                ],
+            ],[
+                "NOMBRE.required" => "El campo nombre es obligatorio",
+                "NOMBRE.max" => "El campo nombre solo permite 100 caracteres",
+                "TELEFONO.required" => "El campo número de teléfono es obligatorio",
+                "TELEFONO.regex" => "El número de teléfono no tiene un formato válido"
+            ]
+            );
 
             $drivers->NOMBRE = $request->input('NOMBRE');
             $drivers->TELEFONO = $request->input('TELEFONO');
             $drivers->save();
+
             return response()->json([
                 'message' => 'El chofer ha sido actualizado',
                 'data' => $drivers
             ], 200);
 
+
+        }catch (ValidationException $e) {
+        return response()->json([
+            'errors' => $e->errors()
+        ], 422);
 
         }catch (\Exception $e) {
             // Retornar un mensaje de error
